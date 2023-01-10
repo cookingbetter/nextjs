50bf522cac1ca760985b743814ca2d846cff3b09
Fedorov Vladimir <65498224+cookingbetter@users.noreply.github.com>
2023-01-10 15:35:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let elements = document.forms[1].elements;
+
+for (let i = 0; i < elements.length; i++) {
+
+elements[i] = '!';
+
+}
+
+elements[0].value='!';
+
+
+
+// function func(){
+        
+//         var elements = document.forms[1].elements;
+
+//         for (var i = 0; i < elements.length; i++) {
+
+//                 elements[i] = '!';
+                
+//                 }
+        
+// }
+
+//Абзац 1<input type="text"><input type="button" value="Delete"><br>
