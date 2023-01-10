b75187bc071f649c9beb12ae1a30326f23e54130
Fedorov Vladimir <65498224+cookingbetter@users.noreply.github.com>
2023-01-10 17:51:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+document.cookie = "user=John"; // update only cookie named 'user'
+alert(document.cookie); 
+
 function diffDays(date1, date2) {
     let year1 = date1.getFullYear(),
         year2 = date2.getFullYear(),
@@ -7,6 +10,7 @@ function diffDays(date1, date2) {
         day2 = date2.getDate();
     let d1 = new Date(year1, month1, day1),
         d2 = new Date(year2, month2, day2),
+        // Смещение для уже полностью очищенных двух локальных дат
         offset1 = d1.getTimezoneOffset(),
         offset2 = d2.getTimezoneOffset(),
         diff = (offset2 - offset1) * 60000,
@@ -14,7 +18,47 @@ function diffDays(date1, date2) {
 
     return days;
 }
-//будуща дата - сегодня
-let strDate = prompt("Введите дату посещения в формате ГГГГ-ММ-ДД");
+
+let strDate = prompt("Введите дату рождения в формате ГГГГ-ММ-ДД");
 let date1 = new Date(strDate + "T00:00");
 document.getElementById("days").value = diffDays(date1, new Date());
+
+setCookie('user', diffDays(date1, new Date()));
+
+function setCookie(name, value) {
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);  
+    document.cookie = updatedCookie;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+// //показать время из куки
+// //alert(document.cookie);
+
+// //если пользователь ввел и нажал
+// //высчитываем значение, записываем в куки
+
+// function func(){
+
+//     let strDate = prompt("Введите дату рождения в формате ГГГГ-ММ-ДД");
+//     let date1 = new Date(strDate + "T00:00");
+
+//     document.getElementById("days").value
+
+//     var now = new Date();
+//     days = getDate()
+//     months = getMonth()
+
+//     setCookie('user', 'John');
+// }
+
