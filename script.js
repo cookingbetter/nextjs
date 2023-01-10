d149d3f98038d1b5329e67a53ddcefdef48b62f8
Fedorov Vladimir <65498224+cookingbetter@users.noreply.github.com>
2023-01-10 16:07:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-document.cookie = 'name=Вася';
-alert(document.cookie); //выведет 'name=Вася'
+function diffDays(date1, date2) {
+    let year1 = date1.getFullYear(),
+        year2 = date2.getFullYear(),
+        month1 = date1.getMonth(),
+        month2 = date2.getMonth(),
+        day1 = date1.getDate(),
+        day2 = date2.getDate();
+    let d1 = new Date(year1, month1, day1),
+        d2 = new Date(year2, month2, day2),
+        offset1 = d1.getTimezoneOffset(),
+        offset2 = d2.getTimezoneOffset(),
+        diff = (offset2 - offset1) * 60000,
+        days = (d2 - d1 + diff) / 86400000;
 
-document.cookie = 'age=25';
-alert(document.cookie); //выведет 'name=Вася; age=25'
-
-
-
-
-// function func(){
-        
-//         var elements = document.forms[1].elements;
-
-//         for (var i = 0; i < elements.length; i++) {
-
-//                 elements[i] = '!';
-                
-//                 }
-        
-// }
-
-//Абзац 1<input type="text"><input type="button" value="Delete"><br>
+    return days;
+}
+//будуща дата - сегодня
+let strDate = prompt("Введите дату посещения в формате ГГГГ-ММ-ДД");
+let date1 = new Date(strDate + "T00:00");
+document.getElementById("days").value = diffDays(date1, new Date());
